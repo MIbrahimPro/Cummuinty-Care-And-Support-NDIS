0ae8ed7b7b59e5622346b3ce4154d4d1e4e871eb
Muhammad Ibrahim <mibrahimpro.1@gmail.com>
2025-12-22 18:12:23 +0500

    client changes phase 2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
     }, []);
 
     return (
-        <Router>
+        <Router basename="/phase1">
             {/* {loading ? ( */}
             {/* <Loader /> */}
             {/* ) : ( */}
